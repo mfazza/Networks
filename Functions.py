0ffe59de834a4da7d80549f105dab782a9a9ca90
mathewsfazza <mathewsfazza@gmail.com>
2017-07-20 19:14:33 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,6 +1,10 @@
 #functions
+from socket import *
+
 
 userid = [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010]
+sPorts = ['a', 12011, 12012, 12013, 12014, 12015, 12016, 12017, 12018, 12019, 12020]
+serverName = gethostname()
 
 def validate (x):
     "Checks to see if user has a valid id matching the ones on the list"
@@ -12,3 +16,25 @@ def validate (x):
             continue
     return isvalid
 
+def get_sport():
+    if len(sPorts) == 0:
+        return 'no port available'
+    else:
+        portNo = sPorts[0]
+        del sPorts[0]
+        return portNo
+
+
+def serverThread(socket1, socket2):
+
+    buffersize = 1024
+    msg1 = ''
+    msg2 = ''
+
+
+    while 1:
+
+        msg1 = socket1.recv(buffersize)
+        msg2 = socket2.recv(buffersize)
+        socket1.send(msg2)
+        socket2.send(msg1)
