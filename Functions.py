1f9543ac50a4ca37114f2a97b02fd24bd6593996
Nickolas Ramos <nickolasoramos@gmail.com>
2017-07-22 18:46:54 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,5 +1,6 @@
 #functions
 from socket import *
+import os
 
 it = 0
 userid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
@@ -29,3 +30,50 @@ def get_sport():
 #         print sPorts.index(it)
 #     except ValueError:
 #         print "not found"
+
+
+
+
+# Nick's portion, open to editing
+# these variables are for testing purposes
+sessionID = 123
+nameOfClient = "Nick"
+loop = True
+
+
+def createChatHistory(sessionID):
+    # makes a file that goes into the logs folder
+    f = open("logs/" + str(sessionID), "w+")
+
+    # write a line to the file
+    f.write("<session_id>\t<from: sending client>\t<chat message> **00 indicates end of history** \n")
+
+    f.close()
+
+
+# where history is the history file, ex '1234.txt'
+def updateHistory(history, sessionID, nameOfClient, message):
+    # now we're going to append the file
+    f = open("logs/" + str(history), "a+")
+
+    f.write(str(sessionID) + "\t\t" + str(nameOfClient) + "\t\t" + str(message) + "\n")
+
+    f.close()
+
+# Is there a logs folder? If not, create one
+if not os.path.exists("logs"):
+    os.makedirs("logs")
+
+createChatHistory(sessionID)
+
+# Here we have a loop for testing the functions.
+# The idea is this loop will run during chat, so we can record messages into a file,
+# then the user can call the history and we could cat the history file to the
+# user's screen.
+print "Update the history!"
+while loop:
+
+    message = raw_input()
+    updateHistory(sessionID, sessionID, nameOfClient, message)
+    if message == "00":
+        loop = False
