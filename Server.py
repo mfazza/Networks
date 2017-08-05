542e140e02de27b61b76b155834fcfd0d0767252
mathewsfazza <mathewsfazza@gmail.com>
2017-08-05 18:12:17 -0500

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,71 +1,146 @@
-''''#server side
-#the server verifies has an ID on the list of subscribers'''
+#server.py
+#Created by Mathews Fazza and Nickolas Ramos
+#
+#This is the message relaying server that works in conjunction with client.py.  As users connect to the listening socket
+#this server creates new sockets to receive their messages.  Those sockets exist in the 'sockets' array.
+#Upon connecting, every user gets assigned their own thread that relays the messages using protocol messages
+#
+#Ctrl + Z will terminate this program on any machine (Windows, Linux, MAC OS)
+
 
 from socket import *
-from Functions import *
+from functions import *
 import thread
 import time
+import threading
+import os
+
+localstring = []                                            #array of string that is used by each thread
+sessions = []                                               #array of individual threads (one per client)
+count = 0                                                   #simple iterator to be used by all threads
 
 
+users = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']  #creates dictionary of sockets objects
+sockets = {}
+for x in range (0, 9):
+    sockets[users[x]] = socket(AF_INET, SOCK_STREAM)        #sockets will be created by the listening socket later
 
 
-
-#function to go into thread
-def __outer_thread__(pNumber, iteration):                   #the function takes a port number, and the index of the array of sockets
-    print "outer thread working"
-
-    individualSockets[iteration] = socket(AF_INET, SOCK_STREAM)
-    individualSockets[iteration].bind(('', pNumber))
-    individualSockets[iteration].listen(1)                  #socket creation, binding, and listening
-    conn, addr = individualSockets[iteration].accept()      #this line took most of my day.  You have to make it explicit that the socket is ready to receive
-
-
-    while 1:                                                #does this forever
-        time.sleep(0.2)                                     #pretty mystic, but it only works if there's a slight delay between iterations
-        fromClient = conn.recv(1024)                        #receive line from client
-        if fromClient == 'request chat with B':             #I worked out a simple validation to make the sockets and the dictionary work
-            if availableUsers.has_key('b'):                 #if key 'b' is in availableusers... That is: if user b is available to chat
-                conn.send("b is available for a chat session")
-                #create chat session
-            else:
-                conn.send("b not available")                #or else, print this
-
-
-count = 0                                                   #this is an iterator for the list of sockets
-forwardPort = 0                                             #this is the port that will be used for the creation of new sockets once the user connects
-individualSockets = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]         #array of sockets (couldn't make it work without explicitly filling it up)
-availableUsers = {}                                         #dictionary of users availble --- [user: port]  -- think of it like a hashmap
-
-serverPort = 12000                                          #The port used by the host to connect to the server
+serverPort = 12000                                          #chosen port
 serverSocket = socket(AF_INET, SOCK_STREAM)                 #creates socket and on the next line, binds it to a port
-serverSocket.bind(('', serverPort))                         #leaving the first member of the tuple as '' denotes the welcoming socket
+serverSocket.bind(('', serverPort))                         #This is the listening socket
 serverSocket.listen(1)                                      #ready to receive data from client
-print "The server is ready to receive"
-while 1:
-    connectionSocket,addr=serverSocket.accept()             #Will indefinitely accept data from clients
-    user_Request = connectionSocket.recv(1024)              #receive (buffer size of 1024 bytes)
-    validation = validate(user_Request)                     #the validation function is in the Functions.py file.  It checks the userID for validation
+
+class BackgroundTask(threading.Thread):                     #This class overrides the __init__  and the run methods
+                                                            #of the threading class
+    def __init__(self, i):
+        threading.Thread.__init__(self)
+        self.i = i
 
 
-    #validation part
-    if validation == True:                                  #if user connects successfully they receive a port number and new thread is created
-        connectionSocket.send("Connected")
-        print 'User ' + user_Request + " has connected to the server"
+    def run(self):
+        while 1:
+            conn, addr = serverSocket.accept()              #Will indefinitely accept data from clients
+            newuser = conn.recv(1024)                       #receive (buffer size of 1024 bytes)
+            validation = validate(newuser)                  #It checks the userID for validation
+            sockets[newuser] = conn                         #passes object to dictionary 'a': socket
 
-        forwardPort = get_sport()                           #call get_sport function to get a port from the available ones
-        connectionSocket.send(str(forwardPort))             #send the port to the client so they know where to connect
+            # validation part
+            if validation == True:                          #if user connects successfully they receive a port number
+                                                            #and new thread is created
 
+                conn.send(protocol[2])                      #sends "CONNECTED" to the user
 
-        availableUsers.update({user_Request: forwardPort})  #updates the dictionary with [the authenticated user: their port number]
-                                                            #then starts new thread that is going to run the __outer__thread__ function
-        thread.start_new_thread(__outer_thread__(forwardPort, count))
+                go_online(newuser)                          #function that logs users into the server
 
-
-        count += 1                                          #updates count
-
-    else:
-        connectionSocket.send("User id not valid\n")
-
+                sessions.append(IndividualTask(newuser, self.i))
+                sessions[self.i].start()                    #once the new user connects, it gets its own thread
 
 
+                self.i += 1                                 #updates count
 
+            else:
+                conn.send("DECLINED\n")
+
+class IndividualTask(threading.Thread):                     #overrides __init__ and run from threading
+    def __init__(self, owner, iterator):
+        threading.Thread.__init__(self)
+        self.iterator = iterator
+        self.owner = owner
+
+    def run(self):
+
+        localstring.append('flks;ddksj')                    #each thread created has one string used for all operations
+        destinationUser = 'z'                               #destination user is used to
+
+        while 1:                                            #Thread will run until the user send a "log out" message
+
+            localstring[self.iterator] = sockets[self.owner].recv(1024) #receives string from user and decides what
+                                                                        #to do based on the protocol
+
+
+            # if protocol == "CHAT_REQUEST"
+            if protocol[3] in localstring[self.iterator]:
+                if localstring[self.iterator][13] != self.owner:        #checks if request being made matches user
+                    sockets[self.owner].send(protocol[5])               #making request.  Sends "UNREACHABLE" if not
+
+                destinationUser = localstring[self.iterator][15]        #receives destination user from protocol
+
+                if destinationUser not in online.keys():                #if destination user not online
+                    sockets[self.owner].send(protocol[5])               #sends "UNREACHABLE" to requesting user
+
+                elif online[self.owner] or online[destinationUser] != 0:    #if users are already engaged in another
+                    sockets[self.owner].send(protocol[5])                   #chat, sends "UNREACHABLE" to them
+                else:
+                    create_chat(self.owner, destinationUser)            #creates chat
+                    chatnumber = online[self.owner]                     #retrives chat number from online dictionary
+                                                                        #then sends CHAT_STARTED to both users
+                    sockets[self.owner].send(protocol[4] + "(" + str(chatnumber) + "-" + self.owner + "-" + destinationUser + ")")
+                    sockets[destinationUser].send(protocol[4] + "(" + str(chatnumber) + "-" + self.owner + "-" + destinationUser + ")")
+
+
+            #if protocol == END_REQUEST
+            elif protocol[6] in localstring[self.iterator]:
+
+                IDDD = int(localstring[self.iterator][12:16])           #gets session number from protocol
+                for key in online.keys():                               #if session number is in online.values()
+                    if online[key] == IDDD:
+                        sockets[key].send(protocol[7] + "(" + str(IDDD) + ")")  #sends notification to users
+
+                for key in online.keys():                               #this loop will reset the values of
+                    if online[key] == IDDD:                             #online[user in chat] to 0, indicating they are
+                        online[key] = 0                                 #online and available
+
+
+            #if protocol == TALK
+            elif protocol[8] in localstring[self.iterator]:
+                destuser = 'x'                                          #initializes destuser
+                sessionid = int(localstring[self.iterator][5:9])        #retrives chat session number from protocol
+
+                if sessionid in online.values():                        #if session id that came in the protocol exists
+                    for key in online.keys():                           #find the users associated with that session id
+                        if online[key] == sessionid and key != self.owner:
+                            print key
+                            destuser = key                              #finds the actual destination user
+                            #writes string to file
+                sockets[destuser].send(localstring[self.iterator][10:]) #sends string to the right user
+                                                                        #then writes to each user's file
+                createChatHistory(self.owner, destuser, sessionid, self.owner, localstring[self.iterator][10:])
+                createChatHistory(destuser, self.owner, sessionid, self.owner, localstring[self.iterator][10:])
+
+            #if protocol == HISTORY_REQ
+            elif protocol[9] in localstring[self.iterator]:
+
+                f = open("logs/" + str(self.owner) + "-" + localstring[self.iterator][14])  #opens file if it exists
+                for line in f:
+                    sockets[self.owner].send(line)                                          #sends each line to client
+                f.close()                                                                   #closes file
+
+            #if protocol = 'log out'
+            elif protocol[11] in localstring[self.iterator]:
+                sockets[self.owner].close()                             #closes socket
+                del online[self.owner]                                  #Makes the user offline
+                break                                                   #breaks from "while 1:" so thread can end
+                #then break
+
+BackgroundTask(count).start()                                           #starts background task.
