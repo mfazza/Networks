ed62a975ac7707f9c346d2715e0d33af3fe71323
mathewsfazza <mathewsfazza@gmail.com>
2017-08-05 18:13:56 -0500

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,79 +1,70 @@
-#functions
+# functions.py
+#Created by Mathews Fazza and Nickolas Ramos
+#
+#This file contains the functions used by all programs in the client-server architecture project
+#Functions are invoked as necessary.  Nothing needs to be done in order to run this program
+
+
 from socket import *
+import random
 import os
 
-it = 0
-userid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
-sPorts = [12010, 12011, 12012, 12013, 12014, 12015, 12016, 12017, 12018, 12019, 12020]
-serverName = gethostname()
-
-def validate (x):
-    "Checks to see if user has a valid id matching the ones on the list"
-    isvalid = False
-    for y in range (0, 9):
-        if (x == userid[y]):
-            isvalid = True
-        else:
-            continue
-    return isvalid
-
-def get_sport():
-    if len(sPorts) == 0:
-        return 'no port available'
-    else:
-        portNo = sPorts[0]
-        del sPorts[0]
-        return portNo
-
-# for it in range (12010, 12022):
-#     try:
-#         print sPorts.index(it)
-#     except ValueError:
-#         print "not found"
+it = 0                                                          #simple iterator
+userid = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']     #list of user ids
 
 
+#dictionary of protocols
+protocol = {1: 'HELLO', 2: 'CONNECTED', 3: 'CHAT_REQUEST', 4: 'CHAT_STARTED', 5: 'UNREACHABLE', 6: 'END_REQUEST',
+            7: 'END_NOTIF', 8: 'TALK', 9: 'HISTORY_REQ', 10: 'HISTORY_RESP', 11: 'log out', 12: 'SNUM', 13: 'HELP'}
+
+online = {}                                                     #dictionary of users online
+serverName = gethostname()                                      #name of server
 
 
-# Nick's portion, open to editing
-# these variables are for testing purposes
-sessionID = 123
-nameOfClient = "Nick"
-loop = True
+def validate(x):                                                #Checks to see if user has a valid id matching userid[]
+    isValid = False
+    if x in userid:
+        if x not in online:
+            isValid = True
+    return isValid
+
+def get_chat_id():                                              #returns a random chat id number from 1000 to 9999
+    return random.randint(1000, 9999)
+
+def go_online(x):                                               #inserts 'x: 0' into online{}
+    online[x] = 0
+
+def create_chat(x, y):                                          #creating a chat, means setting the number in
+    chatid = get_chat_id()                                      #online{} from 0 to a random returned by the function
+    online[x] = chatid
+    online[y] = chatid                                          #both users involved in the same chat get the same number
 
 
-def createChatHistory(sessionID):
-    # makes a file that goes into the logs folder
-    f = open("logs/" + str(sessionID), "w+")
+
+def createChatHistory(sender, receiver, sessionID, nameOfClient, message):  #checks to see if a folder exists, if the files exist,
+                                                                            #and furthermore, creates or appends files with messages.
+    if os.path.exists("logs") == False:
+        os.makedirs("logs")
+
+    # If the file doesn't exist, makes a file that goes into the logs folder
+    if not os.path.exists(str(sender) + "-" + str(receiver)):
+        f = open("logs/" + str(sender) + "-" + str(receiver), "a+")
 
     # write a line to the file
-    f.write("<session_id>\t<from: sending client>\t<chat message> **00 indicates end of history** \n")
+    if os.path.exists("logs/" + str(sender) + "-" + str(receiver)):
+        f.write("<" + str(sessionID) + ">" + "\t" + "<" + str(nameOfClient) + ">" + "\t" + str(message) + "\n")
 
     f.close()
 
+#gethelp function is just a way to aid users in typing the commands
+def gethelp():
+    print "Each command can be typed at any moment.  Commands should be typed in caps lock using the following format: COMMAND(arguments).\n"
+    print "CHAT_REQUEST(a-b) where a and b are users and should be lower case separated by a dash\n"
+    print"END_REQUEST(####) where #### is the four digit numebr identifying the chat session number\n"
+    print "HISTORY_REQUEST(a-b) where a and b are users and should be lower case separated by a dash\n"
+    print "SNUM displays the number of current chat session if there is one.  The number 77 indicates there is no session\n"
+    print "log out   log out, typed in lower case, separated by a space, disconnects the user from the server"
 
-# where history is the history file, ex '1234.txt'
-def updateHistory(history, sessionID, nameOfClient, message):
-    # now we're going to append the file
-    f = open("logs/" + str(history), "a+")
 
-    f.write(str(sessionID) + "\t\t" + str(nameOfClient) + "\t\t" + str(message) + "\n")
 
-    f.close()
 
-# Is there a logs folder? If not, create one
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-
-createChatHistory(sessionID)
-
-# Here we have a loop for testing the functions.
-# The idea is this loop will run during chat, so we can record messages into a file,
-# then the user can call the history and we could cat the history file to the
-# user's screen.
-print "Update the history!"
-while loop:
-
-    message = raw_input()
-    updateHistory(sessionID, sessionID, nameOfClient, message)
-    if message == "00":
-        loop = False
